dfe1bce06e39cf6eb583ed309ae9d5bda43f7587
stack07142 <stack.07142@gmail.com>
2016-11-06 23:34:00 +0900

    HappyAlgo#3_ProgramP

---
+++ HappyAlgo#3_ProgramP/src/Main.java
@@ -0,0 +1,2 @@
+
+public class Main {	public static void main(String[] args) { System.out.print(a); System.out.print((char) 13); System.out.print((char) 34); System.out.print(a); System.out.print((char) 34);} static String a= "public class Main {	public static void main(String[] args) { System.out.print(a); System.out.print((char) 13); System.out.print((char) 34); System.out.print(a); System.out.print((char) 34);} static String a= "; }
